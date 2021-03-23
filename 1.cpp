8ba9d6f072a53a70cc4fca388c88efb271bd3736
1720648723 <1720648723@qq.com>
2021-03-23 11:38:28 +0800

    add getVM api

--- 1.cpp
+++ 1.cpp
@@ -24,6 +24,25 @@ struct Server
 
 struct VM
 {
+    string name;
+    int cpu;
+    int memory;
+    //对于双节点VM会存在将cpu和memory平分的情况，故引入cpuHalf和memoryHalf
+    //node==1 表示双节点
+    int node;
+    int cpuHalf;
+    int memoryHalf;
+    VM() {};
+
+    VM(string _name, int _cpu, int _memory, int _node,int _cpuHalf,int _memoryHalf)
+    {
+        name = _name;
+        cpu = _cpu;
+        memory = _memory;
+        node = _node;
+        cpuHalf = _cpuHalf;
+        memoryHalf = _memoryHalf;
+    }
 };
 
 //比较两个服务器的优先级，按cpu数、内存大小、每日价格、成本的顺序依次判断
@@ -90,7 +109,26 @@ void sortServerList(VMList &list) {}
 void InsertToServerList(VMList &list, Server s) {}
 
 //从标准输入输出读到一个虚拟机信息，返回一个VM
-VM getVM() {}
+VM getVM() {
+    VM oneVM;
+    getchar();
+    char str[100];
+    scanf("%s, ",str);
+    int i=0;
+    for(i=0;str[i]!='\0';i++);
+    str[--i]='\0';
+    scanf("%d, %d, %d",&oneVM.cpu,&oneVM.memory,&oneVM.node);
+    string str2 = string(str);
+    if(oneVM.node){
+        oneVM.cpuHalf=oneVM.cpu/2;
+        oneVM.memoryHalf=oneVM.memory/2;
+    }else{
+        oneVM.cpuHalf=0;
+        oneVM.memoryHalf=0;
+    }
+    VM ans =VM(str2,oneVM.cpu,oneVM.memory,oneVM.node,oneVM.cpuHalf,oneVM.memoryHalf);
+    return ans;
+}
 
 //获取一天的需求
 void getRequestList(VMList &list) {}
@@ -98,7 +136,8 @@ void getRequestList(VMList &list) {}
 int main()
 {
     Server myServer = Server("gta5", 1, 1, 1, 1);
-
+    VM testVM = VM();
+    getVM();
     system("pause");
     return 0;
 }
