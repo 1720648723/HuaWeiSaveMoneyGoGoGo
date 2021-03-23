ebc283c9c98a1e8e1672b720369c14d511dc5139
xendxby <xendxby@163.com>
2021-03-23 11:15:30 +0800

    Some pots

--- 1.cpp
+++ 1.cpp
@@ -9,16 +9,22 @@ struct Server
     int memory;
     int cost;
     int dayCost;
+    int index;
+
+    int pears[2];
 
     Server() {}
 
-    Server(string _name, int _cpu, int _memory, int _cost, int _dayCost)
+    Server(string _name, int _cpu, int _memory, int _cost, int _dayCost, int _index)
     {
         name = _name;
         cpu = _cpu;
         memory = _memory;
         cost = _cost;
         dayCost = _dayCost;
+        index = _index;
+        pears[0] = 0;
+        pears[1] = 0;
     }
 };
 
@@ -43,13 +49,37 @@ void getAllServer(VMList &list) {}
 void sortServerList(VMList &list) {}
 
 //将s插入到list（有序）中按serverLess顺序的位置
-void InsertToServerList(VMList &list, Server s) {}
+//服务器index值为index
+void InsertToServerList(VMList &list, Server s, int index) {}
 
 //从标准输入输出读到一个虚拟机信息，返回一个VM
 VM getVM() {}
 
 //获取一天的需求
 void getRequestList(VMList &list) {}
+
+//将vm插入slist和vlist中
+//slist保证新插入的vm在满足硬件要求的前提下，尽量处于slist的靠前位置和A（peer[0]）节点
+//将slist中的该节点值改为vlist中vm对应的下标（从1开始）
+//vlist保证新插入vm在vlist末位，编号从1开始，即vlist[0]为空虚拟机
+//注意加入双节点虚拟机时特殊处理
+void addVM(serverList &slist, VMList &vlist, VM vm) {}
+
+//将slist中若干个peer迁移至slist尽量靠前的位置（且满足硬件要求）
+//最多迁移maxTimes次
+//返回一个vector<int>，下标为0~2n-1，其中0为原服务器index，
+//注意双节点虚拟机暂不考虑迁移
+vector<int> migrateVM(serverList &slist, VMList &vlist, int maxTimes) {}
+
+//根据request，检查slist是否需要购买服务器
+//message为可购买的服务器信息，按serverLess升序排序
+//若需要购买服务器，则在slist中添加若干服务器
+//返回一个serverList，内容为添加的服务器列表
+serverList purchaseServer(serverList &slist, VMList &request, serverList &message) {}
+
+void solve()
+{
+}
 
 int main()
 {
